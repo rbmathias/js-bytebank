738fdb9270d7330c7bb1d8a51bd599ea63832c14
rbmathias <r-mathias@hotmail.com>
2022-08-09 12:53:29 -0300

    Envio de arquivos iniciais projeto ByteBAnk

---
+++ index.js
@@ -0,0 +1,14 @@
+import { Cliente } from "./Cliente.js";
+import { ContaCorrente } from "./ContaCorrente.js";
+
+const cliente1 = new Cliente("Rodrigo", 11122233309);
+const cliente2 = new Cliente("Damy", 22233344407);
+
+const contaCorrenteRodrigo = new ContaCorrente(1001, cliente1);
+const conta2 = new ContaCorrente(1002, cliente2);
+
+contaCorrenteRodrigo.depositar(600);
+let valor = 200;
+contaCorrenteRodrigo.transferir(valor, conta2);
+
+console.log(ContaCorrente.numeroDeContas);
